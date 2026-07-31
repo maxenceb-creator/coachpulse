2eec93d36564870ea1775de79a49312577627e90
Sylvain Ostard <sylvain.ostard@gmail.com>
2026-07-31 16:17:37 +0200

    refactor: extract methodologie embedded colors

--- sw.js
+++ sw.js
@@ -9,6 +9,7 @@ const CORE_ASSETS = [
   './icons/icon-180.png', './icons/icon-192.png', './icons/icon-512.png', './icons/icon-1024.png',
   './pages/coach-stats.html', './pages/methodologie.html', './pages/presences.html', './pages/tests-techniques.html', './pages/tests-athletiques.html', './pages/data-hub.html', './pages/admin-database.html', './pages/suivi-medical.html', './pages/player-profile.html', './pages/team-profile.html',
   './pages/coach-stats/heatmap-tooltips.css', './pages/coach-stats/heatmap-tooltips.js',
+  './pages/methodologie/embedded-colors.css', './pages/methodologie/embedded-colors.js',
   './pages/player-profile/playerProfile.css', './pages/player-profile/playerProfileData.js', './pages/player-profile/playerProfileFilters.js', './pages/player-profile/playerProfileStats.js', './pages/player-profile/playerProfileCompare.js', './pages/player-profile/playerProfileRender.js', './pages/player-profile/playerProfile.js',
   './pages/team-profile/teamProfile.css', './pages/team-profile/teamProfileData.js', './pages/team-profile/teamProfileFilters.js', './pages/team-profile/teamProfileMetrics.js', './pages/team-profile/teamProfileCharts.js', './pages/team-profile/teamProfileUI.js', './pages/team-profile/teamProfile.js',
   './connectors/fichesJoueusesConnector.js', './connectors/presencesConnector.js',
